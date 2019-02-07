2f5e480a7f7e6b57c2bf0f4e0ea51ef79b9180e1
Guillaume Pagesy <blackduanpagesy@hotmail.com>
2019-02-07 23:28:15 +0100

    re modif

--- compet/src/app/app.module.ts
+++ compet/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CompetitionComponent } from './competition/competition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetitionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'competition',
+        component: CompetitionComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
