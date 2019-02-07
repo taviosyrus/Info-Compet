dbd2a11798e494aca35b71400bab7567dba91662
Komi octave <34370469+taviosyrus@users.noreply.github.com>
2019-02-07 19:51:32 +0100

    jeudi le 7 fevrier 2019 mon premier commit GBLOKPO Komi Octave

--- compet/src/app/app.module.ts
+++ compet/src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    AcceuilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+      path: 'login',
+      component: ConnexionComponent
+      },
+      {
+        path: '',
+        component: AcceuilComponent
+        }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
