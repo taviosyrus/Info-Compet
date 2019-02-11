952d82cccf9379ed285be14284e7b30afedadec6
Komi octave <34370469+taviosyrus@users.noreply.github.com>
2019-02-11 21:37:45 +0100

    GBLOKPO Komi Octave 11 fevrier 2019 mon commit 2

---
+++ compet/src/app/listparticipant/listparticipant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listparticipant',
+  templateUrl: './listparticipant.component.html',
+  styleUrls: ['./listparticipant.component.css']
+})
+export class ListparticipantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
